cb4e9f6f7ad6832827dbe6c4f297b11497ee8688
Damian Maclennan <damian@damianm.com>
2014-09-30 09:43:02 +1000

    environmant var

--- AwsSampleSite/Controllers/HomeController.cs
+++ AwsSampleSite/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +11,9 @@ namespace AwsSampleSite.Controllers
     {
         public ActionResult Index()
         {
+
+            var env = ConfigurationManager.AppSettings["EnvironmentName"];
+            ViewBag.env = env;
             return View();
         }
 
