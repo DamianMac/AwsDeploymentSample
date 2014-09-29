8fb490e87b92d19ecbc7aec020f2ecc21e6c018c
Damian Maclennan <damian@damianm.com>
2014-09-30 09:13:47 +1000

    init

---
+++ AwsSampleSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AwsSampleSite.MvcApplication" Language="C#" %>
---
+++ AwsSampleSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AwsSampleSite.Startup))]
+namespace AwsSampleSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
